9463cb11bc47013c7e9603bdea483c82b5965e23
Yoon-Haeng-Heo <submailid96@gmail.com>
2020-08-20 09:59:48 +0900

    fix the alignment and padding cardview

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -26,44 +26,53 @@ class AuthPage extends StatelessWidget {
                 width: 200,
                 height: 200,
                 color: Colors.blue,
-              ),//gif가 들어갈 공간
+              ), //gif가 들어갈 공간
               Stack(
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.all(size.width*0.05), //패딩 추가
+                    padding: EdgeInsets.all(size.width * 0.05), //패딩 추가
                     child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
                       elevation: 8, //그림자 추가
                       child: Form(
                         key: _formKey, //unique한 id라고 생각하면 될 듯
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,   //정렬 왼쪽으로
-                          children: <Widget>[
-                            TextFormField(
-                              controller: _emailController,
-                              decoration: InputDecoration(
-                                  icon: Icon(Icons.account_circle),
-                                  labelText: "Email"),
-                              validator: (String value){  //user가 뭔가 작성을 하면 value가 맞는 데이터인지 체크해주는 것
-                                if(value.isEmpty) {
-                                  return "Please input correct Email"; // error message
-                                }
-                                return null;
-                                },
-                            ),
-                            // animation이 자동으로 생김
-                            TextFormField(
-                              controller: _pwController,
-                              decoration: InputDecoration(icon: Icon(Icons.vpn_key),labelText: "Password"),
-                                validator: (String value){
-                                  if(value.isEmpty) {
-                                    return "Please input correct Password"; // error message
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 12.0, right: 12.0, top: 12.0, bottom: 36.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            //정렬 왼쪽으로
+                            children: <Widget>[
+                              TextFormField(
+                                controller: _emailController,
+                                decoration: InputDecoration(
+                                    icon: Icon(Icons.account_circle),
+                                    labelText: "Email"),
+                                validator: (String value) {
+                                  //user가 뭔가 작성을 하면 value가 맞는 데이터인지 체크해주는 것
+                                  if (value.isEmpty) {
+                                    return "Please input correct Email"; // error message
                                   }
                                   return null;
-                                }
-                            ),
-                            Container(height: 10),
-                            Text("Forgot Password?"),
-                          ],
+                                },
+                              ),
+                              // animation이 자동으로 생김
+                              TextFormField(
+                                  controller: _pwController,
+                                  decoration: InputDecoration(
+                                      icon: Icon(Icons.vpn_key),
+                                      labelText: "Password"),
+                                  validator: (String value) {
+                                    if (value.isEmpty) {
+                                      return "Please input correct Password"; // error message
+                                    }
+                                    return null;
+                                  }),
+                              Container(height: 10),
+                              Text("Forgot Password?"),
+                            ],
+                          ),
                         ),
                       ),
                     ),
